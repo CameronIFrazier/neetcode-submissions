9e0f5890d9ec98fc8dd2d18a7219a55a4427a1d8
CameronIFrazier <113954099+CameronIFrazier@users.noreply.github.com>
2026-09-20 13:46:03 -0700

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+
+        let left = 1;
+        let right = Math.max(...piles);
+        let k = Math.floor((left+right)/2);
+        //binary serach over k (speed)
+        let bestTime = 1;
+        while(left<=right){
+            let hours = 0;
+            for(let i =0; i<piles.length;i++){
+                hours+=Math.ceil(piles[i]/k);               
+            }
+            if(hours>h){ //took to long, increase speed
+            left=k+1;
+            } else if(hours<h){ //too fast, slow down
+            right=k-1;  
+            bestTime = k;
+            } else {
+            right=k-1;  
+            bestTime = k;
+            }
+            k = Math.floor((left+right)/2);
+        }
+        return bestTime;
+    }
+}
