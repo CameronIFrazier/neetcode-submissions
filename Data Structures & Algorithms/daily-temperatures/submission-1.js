62309f70ad3f17cef81ec9b4d2502acca7ae5a9e
CameronIFrazier <113954099+CameronIFrazier@users.noreply.github.com>
2026-09-21 14:26:17 -0700

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        let results = new Array(temperatures.length).fill(0); 
+        let stack = [];
+       for(let i =0; i<temperatures.length;i++){
+        while(stack.length!=0 && (temperatures[i]>temperatures[stack[stack.length-1]])){
+            let idx  = stack.pop();
+            results[idx] = i -idx;
+        }
+        stack.push(i);
+        
+    }
+    return results;
+}
+}
